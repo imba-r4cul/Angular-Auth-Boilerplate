81eb09fd41ffb70cd7238081350a1f2986453e8b
r4cul <raculxd@gmail.com>
2026-05-07 21:04:43 +0800

    Forget Password Fixed 2.0

--- angular-21-boilerplate/src/app/account/register.component.ts
+++ angular-21-boilerplate/src/app/account/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
@@ -17,7 +17,8 @@ export class RegisterComponent implements OnInit {
         private route: ActivatedRoute,
         private router: Router,
         private accountService: AccountService,
-        private alertService: AlertService
+        private alertService: AlertService,
+        private cdr: ChangeDetectorRef
     ) { }
 
     ngOnInit() {
@@ -39,6 +40,7 @@ export class RegisterComponent implements OnInit {
 
     onSubmit() {
         this.submitted = true;
+        this.cdr.detectChanges();
 
         // reset alerts on submit
         this.alertService.clear();
@@ -49,16 +51,24 @@ export class RegisterComponent implements OnInit {
         }
 
         this.submitting = true;
+        this.cdr.detectChanges();
+
         this.accountService.register(this.form.value)
             .pipe(first())
             .subscribe({
                 next: () => {
                     this.alertService.success('Registration successful, please check your email for verification instructions', { keepAfterRouteChange: true });
-                    this.router.navigate(['../login'], { relativeTo: this.route });
+                    // Stay on register page so the user can see the fake backend verification link alert
+                    // this.router.navigate(['../login'], { relativeTo: this.route });
+                    this.submitting = false;
+                    this.submitted = false;
+                    this.form.reset();
+                    this.cdr.detectChanges();
                 },
                 error: error => {
                     this.alertService.error(error);
                     this.submitting = false;
+                    this.cdr.detectChanges();
                 }
             });
     }
