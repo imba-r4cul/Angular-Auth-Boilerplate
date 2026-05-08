d7278e1a40443a42e4ba83af24a4832da1d6375e
r4cul <raculxd@gmail.com>
2026-05-09 04:32:56 +0800

    Fix undefined refresh token error

--- backend/accounts/accounts.controller.ts
+++ backend/accounts/accounts.controller.ts
@@ -43,6 +43,7 @@ function authenticate(req: any, res: any, next: any) {
 
 function refreshToken(req: any, res: any, next: any) {
   const token = req.cookies.refreshToken;
+  if (!token) return res.status(400).json({ message: 'Token is required' });
   const ipAddress = req.ip;
   accountService.refreshToken({ token, ipAddress })
     .then(({ refreshToken, ...account }: any) => {
@@ -61,6 +62,7 @@ function revokeTokenSchema(req: any, res: any, next: any) {
 
 function revokeToken(req: any, res: any, next: any) {
   const token = req.body.token || req.cookies.refreshToken;
+  if (!token) return res.status(400).json({ message: 'Token is required' });
   const ipAddress = req.ip;
 
   if (!token) return res.status(400).json({ message: 'Token is required' });
