cf23eae94a2a8f024b58db9a8f12d3907627ce91
r4cul <raculxd@gmail.com>
2026-05-10 06:05:10 +0800

    Switch Swagger to JSON for perfect Vercel compatibility

--- backend/_helpers/swagger.ts
+++ backend/_helpers/swagger.ts
@@ -1,10 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-
-import path from 'path';
-const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+import swaggerDocument from '../swagger.json';
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
