1ca7e2b53ab4f35f70f9a4c377a24aea3157f1f7
r4cul <raculxd@gmail.com>
2026-05-10 05:47:19 +0800

    Fix Swagger path using dirname

--- backend/_helpers/swagger.ts
+++ backend/_helpers/swagger.ts
@@ -4,7 +4,7 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
 import path from 'path';
-const swaggerDocument = YAML.load(path.join(process.cwd(), 'swagger.yaml'));
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
